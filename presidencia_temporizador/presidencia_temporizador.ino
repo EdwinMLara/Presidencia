e91f7cd152db140ddf7f059f17e812496448bbb5
EdwinMLara <emlara35@gmail.com>
2020-05-08 13:05:24 -0500

    Configurando la pantalla para mostrar el control de tiempo y otras mamadas

--- presidencia_temporizador/presidencia_temporizador.ino
+++ presidencia_temporizador/presidencia_temporizador.ino
@@ -1,4 +1,8 @@
+#include <Wire.h>
 #include<string.h> 
+#include <LiquidCrystal_PCF8574.h>
+
+LiquidCrystal_PCF8574 lcd(0x27);
 
 unsigned long startmillis;
 unsigned long currentmillis;
@@ -9,8 +13,7 @@ unsigned long tiempo_apagado = 2;
 unsigned long tiempo_encendido2 = 5;
 unsigned long tiempo_apagado2 = 13;
 boolean banderas[4];
-boolean gt_val;
-
+boolean gt_val,status_rele,aux_status_rele;
 unsigned long desfaseTime = 0;
 
 void iniciar_banderas(boolean *banderas, uint8_t tam){
@@ -18,6 +21,58 @@ void iniciar_banderas(boolean *banderas, uint8_t tam){
   for(i=0;i<=tam-1;i++){
     banderas[i] = true;
   }
+}
+
+void config_lcd(){
+  int error;
+
+  while (!Serial);
+
+  Serial.println("Dose: check for LCD");
+  
+  Wire.begin();
+  Wire.beginTransmission(0x27);
+  error = Wire.endTransmission();
+  Serial.print("Error: ");
+  Serial.print(error);
+
+  if (error == 0) {
+    Serial.println(": LCD found.");
+
+  } else {
+    Serial.println(": LCD not found.");
+  } 
+
+  lcd.begin(16, 2 );
+  lcd.setBacklight(100);
+  lcd.clear();
+}
+
+
+void pintar_Pantalla(char str[]){
+    
+  String estado_rele;
+  if(status_rele != aux_status_rele){
+    aux_status_rele = status_rele;
+    lcd.clear();
+    if(status_rele) 
+      estado_rele = "Encendido";
+    else
+      estado_rele = "Apagado";
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("Municipio  Uriangato");
+  lcd.setCursor(0, 1);
+  char fecha[20] = "Tiempo - ";
+  strcat(fecha,str);
+  lcd.print(fecha);
+  
+  estado_rele = "Estado: " + estado_rele;
+  lcd.setCursor(0, 2);
+  lcd.print(estado_rele);
+  lcd.setCursor(0,3);
+  lcd.print("Por: TIC's ");
 }
 
 void Seguntos2Time(unsigned long tiempo){
@@ -39,16 +94,21 @@ void Seguntos2Time(unsigned long tiempo){
     itoa(seg,aux,10);
     strcat(str,aux);
     Serial.println(str);
+   
+    pintar_Pantalla(str);
     /*str_time = strdup(str);
     return str_time;*/
 }
 void setup() {
+  config_lcd();
   Serial.begin(9600); 
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
   pinMode(7,INPUT_PULLUP);
   startmillis = millis();
   digitalWrite(3,HIGH);
+  status_rele = true;
+  aux_status_rele = false;
   digitalWrite(4,LOW);
 
   iniciar_banderas(banderas,4);
@@ -65,24 +125,28 @@ void loop() {
      
   if((countTime >= tiempo_encendido) && banderas[0]){
     digitalWrite(3,LOW);
+    status_rele = false;
     digitalWrite(4,HIGH);
     Serial.println("Entro en 1");
     banderas[0] = false;
   }
   if((countTime >= (tiempo_encendido + tiempo_apagado)) && banderas[1]){
     digitalWrite(3,HIGH);
+    status_rele = true;
     digitalWrite(4,LOW);
     Serial.println("Entro en 2");
     banderas[1] = false;
   }
   if((countTime >= (tiempo_encendido + tiempo_apagado + tiempo_encendido2)) && banderas[2]){
     digitalWrite(3,LOW);
+    status_rele = false;
     digitalWrite(4,HIGH);
     Serial.println("Entro en 3");
     banderas[2] = false;
   }
   if((countTime >= (tiempo_encendido + tiempo_apagado + tiempo_encendido2 + tiempo_apagado2)) && banderas[3]){
     digitalWrite(3,HIGH);
+    status_rele = true;
     digitalWrite(4,LOW);
     banderas[3] = false;
     startmillis = millis();
